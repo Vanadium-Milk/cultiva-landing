fc1ee14dcd45c0915bd22c6b507a02e763d8bfa2
Leah Castro <leahvcm.01@gmail.com>
2026-03-06 13:59:25 -0600

    Add base

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     port: 80,
     host: true,
-    allowedHosts: ["proyectocultiva.org"],
-  }
+    allowedHosts: ['proyectocultiva.org'],
+  },
+  base: '/',
 })
