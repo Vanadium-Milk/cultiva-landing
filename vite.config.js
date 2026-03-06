d4e6b87070dd55ca268bb2234f75423dc4c16224
Leah Castro <leahvcm.01@gmail.com>
2026-03-06 14:55:38 -0600

    Add icon -Updated repository link -Removed port specification

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 80,
     host: true,
     allowedHosts: ['proyectocultiva.org'],
   },
