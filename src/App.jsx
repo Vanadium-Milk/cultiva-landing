978113fa42f3f0e7b2db0cddb977a06821782676
Leah Castro <leahvcm.01@gmail.com>
2026-03-20 03:32:40 -0600

    Update logo -Updated original logo -Updated setup information

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { Activity, ArrowRight, Brain, CheckSquare, Cpu, Database, Download, Droplets, FileText, Github, Leaf, Maximize, Menu, Package, Plug, Power, RotateCcw, Server, Sprout, Terminal, Usb, User, Wifi, Wrench, X } from 'lucide-react';
 import { useRef, useState } from 'react';
 import './styles.css';
+import { Logo } from './components/logo';
 
 /*
 CONFIG Y DATOS DEL PROYECTO
 */
 const PROJECT_INFO = {
   repoUrl: "https://github.com/Vanadium-Milk/cultiva-client",
-  fipi: "DUR-SI-1833",
-  university: "Universidad Politécnica de Durango",
   authors: ["Leah Valentina Castro Moreno", "José Ignacio Contreras Méndez", "Octavio García Celaya"],
   advisor: "MC Christian Ríos Chavarría",
   videoApp: "/app-demo.mp4" 
@@ -21,10 +20,7 @@ const Navbar = () => {
     <>
       <nav className="navbar">
         <div className="nav-container">
-          <a href="#" className="logo">
-            <Sprout size={28} color="#2ecc71" />
-            CULTIVA
-          </a>
+          <Logo/>
           
           <div className="nav-links">
             <a href="#hero" className="nav-link">Inicio</a>
@@ -72,9 +68,6 @@ const Hero = () => {
   return (
     <section id="hero" className="container hero">
       <div className="hero-content">
-        <div style={{ display:'inline-block', padding: '5px 15px', background: 'rgba(46, 204, 113, 0.15)', borderRadius: '20px', color: '#145a32', fontWeight: '600', fontSize: '0.8rem', marginBottom: '15px', border: '1px solid rgba(46, 204, 113, 0.3)' }}>
-          Proyecto FIPI: {PROJECT_INFO.fipi}
-        </div>
         
         <h1>Sistema Embebido Autónomo con IA Multimodal</h1>
         
@@ -183,7 +176,7 @@ const Innovations = () => {
             <div style={{background: '#d5f5e3', padding: '10px', borderRadius: '12px'}}><Database size={24} color="#2ecc71"/></div>
             <h3 style={{fontSize: '1.2rem', margin: 0}}>Bajo Costo y Reciclaje</h3>
           </div>
-          <p style={{fontSize: '0.9rem', flex: 1}}>Costo total<strong> adaptable </strong>a la necesidad de cada usuario . Estructura con PVC hidráulico y materiales reutilizados.</p>
+          <p style={{fontSize: '0.9rem', flex: 1}}>Costo total<strong> adaptable </strong>a la necesidad de cada usuario. Estructura con PVC sanitario y materiales reutilizados.</p>
         </div>
         <div className="glass-card" style={{textAlign: 'left', display: 'flex', flexDirection: 'column', height: '100%'}}>
           <div style={{display: 'flex', alignItems: 'center', gap: '10px', marginBottom: '15px'}}>
@@ -198,18 +191,51 @@ const Innovations = () => {
 };
 
 const GUIDE_STEPS = [
-  { id: 1, title: "Preparar el hardware", desc: "Conseguir cualquier computadora con arquitectura soportada (x86_64 o aarch64).", icon: <Server size={24}/> },
+  { id: 1, title: "Preparar el hardware", desc: `Consigue cualquier computadora con arquitectura x86_64 o arm64.
+
+Requisitos mínimos:
+    - RAM 512MB
+    - Procesador 1 GHz single-core
+    - Almacenamiento 25GB
+    - Dos puertos USB (puede usarse adaptador)
+    - Conectividad por Wifi o ethernet (preferible ethernet)`, icon: <Server size={24}/> },
   { id: 2, title: "Instalar el Sistema Operativo", desc: "Instalar Debian 13 (incluido con la Raspberry Pi). No se necesita entorno gráfico.", icon: <Terminal size={24}/> },
-  { id: 3, title: "Descargar los archivos", desc: "Descargar los archivos de instalación en la computadora.", icon: <Download size={24}/> },
-  { id: 4, title: "Instalar el paquete", desc: "Ejecutar: sudo dpkg -i _nombre del archivo_", icon: <Package size={24}/> },
-  { id: 5, title: "Utilidades CLI", desc: "Una vez instalado se podrán usar las utilidades incluidas: 'sudo cultiva-cli configure' para iniciar la configuración y 'sudo cultiva-cli compile' para compilar código directo al Arduino.", icon: <Wrench size={24}/> },
-  { id: 6, title: "Conectar la placa", desc: "Conectar una placa Arduino por medio de USB (recomendación: Arduino UNO).", icon: <Usb size={24}/> },
-  { id: 7, title: "Iniciar Sesión", desc: "Una vez que se ejecute la utilidad de configuración, seguir los pasos e iniciar sesión con la cuenta (en caso de no contar con una, puede realizar el registro ahí mismo).", icon: <User size={24}/> },
-  { id: 8, title: "Conectar los sensores", desc: "Conecta los sensores y actuadores que se vayan a instalar haciendo uso los puertos recomendados.", icon: <Plug size={24}/> },
+  { id: 3, title: "Descargar los archivos", desc: `Descargar los archivos de instalación en la computadora.
+
+Para sesiones SSH o terminal emulada copia y ejecuta el siguiente comando:
+wget https://github.com/Vanadium-Milk/cultiva-client/releases/download/Debian/cultiva_v1.0.0-beta_amd64.deb
+  
+(reemplaza 'amd64' por la arquitectura de tu procesador si hace falta)`, icon: <Download size={24}/> },
+  { id: 4, title: "Instalar el paquete", desc: `Instala el paquete usando el comando:
+sudo dpkg -i cultiva_v1.0.0-beta_amd64.deb
+
+Si tienes dependencias faltantes ejecuta:
+sudo apt-get -f install
+
+Una vez instalado se podrán usar las utilidades incluidas:
+    - 'sudo cultiva-cli configure' para iniciar la configuración
+    - 'sudo cultiva-cli compile' para compilar código directo al Arduino.`, icon: <Package size={24}/> },
+  { id: 5, title: "Conectar la placa", desc: "Conectar una placa Arduino por medio de USB (recomendación: Arduino UNO).", icon: <Usb size={24}/> },
+  { id: 6, title: "Conectar los sensores", desc: `Conecta los sensores y actuadores que se vayan a instalar haciendo uso los puertos recomendados:
+    - DHT11 y DHT22 - Digital 2
+    - Sensor de humedad de suelos - Analog 0
+    - Sensor de luminosidad - Analog 1
+    - Sensor de Co2 - SCL y SCA (Intefaz I2C)
+    - Sensores de PH aún no tienen soporte
+
+Contáctanos si necesitas integración con hardware adicional`, icon: <Plug size={24}/> },
+  { id: 7, title: "Utilidades CLI", desc: `Inicia la configuración guiada con el comando 'sudo cultiva-cli configure' (necesitas privilegios de administrador)
+Sigue el proceso tal como se indica `, icon: <Wrench size={24}/> },
+  { id: 8, title: "Iniciar Sesión", desc: "Una vez que se ejecute la utilidad de configuración, seguir los pasos e iniciar sesión con la cuenta.\nEn caso de no contar con una, puedes realizar el registro dentro de la configuración guiada.", icon: <User size={24}/> },
   { id: 9, title: "Seleccionar componentes", desc: "En la configuración guiada, seleccionar los sensores y actuadores que se hayan conectado.", icon: <CheckSquare size={24}/> },
-  { id: 10, title: "Compilación automática", desc: "Dejar que el proceso termine y compile el código automáticamente en el Arduino.", icon: <Cpu size={24}/> },
-  { id: 11, title: "Resolución de problemas", desc: "Si existe algún problema en el proceso de compilación, repetirlo ejecutando 'sudo cultiva-cli compile'.", icon: <RotateCcw size={24}/> },
-  { id: 12, title: "Reiniciar", desc: "Una vez terminado el proceso, reiniciar la computadora.", icon: <Power size={24}/> },
+  { id: 10, title: "Compilación automática", desc: `Dejar que el proceso termine y compile el código automáticamente en el Arduino.
+
+Usamos librerías y utilidades de terceros de código abierto, debes permitir la instalación de estas para continuar el proceso`, icon: <Cpu size={24}/> },
+  { id: 11, title: "Resolución de problemas", desc: `Estos son algunos de los problemas más comunes:
+    - Si existe algún problema en el proceso de compilación, repetirlo ejecutando 'sudo cultiva-cli compile'.
+    - Algunas placas Arduino de imitación no se detectan automáticamente, por lo que debes escribir manualmente su FQBN, algunos de los más comunes son: 'arduino:avr:uno', 'arduino:avr:nano', 'arduino:avr:mega', utiliza el más similar
+    - Si necesitas editar un parámetro de configuración esta se guarda en /etc/cultiva/settings.toml, edita el texto o repite el proceso para cambiarlo.`, icon: <RotateCcw size={24}/> },
+  { id: 12, title: "Reiniciar", desc: "Una vez terminado el proceso habilita el servicio usando 'sudo systemctl enable --now cultiva.service'.", icon: <Power size={24}/> },
   { id: 13, title: "Comprobar los logs", desc: "Puede comprobar los logs usando: 'sudo journalctl -xeu cultiva.service'.", icon: <FileText size={24}/> }
 ];
 
@@ -283,10 +309,9 @@ const Footer = () => {
     <footer className="footer">
       <div className="container">
         <h3 style={{color: '#145a32', display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10px', marginBottom: '15px'}}>
-          <Sprout size={24} /> CULTIVA
+          <Logo></Logo>
         </h3>
         <div style={{borderTop: '1px solid rgba(0,0,0,0.05)', borderBottom: '1px solid rgba(0,0,0,0.05)', padding: '20px 0', margin: '20px 0'}}>
-            <p style={{fontWeight: 'bold', color: '#145a32', marginBottom: '10px'}}>{PROJECT_INFO.university}</p>
             <div style={{fontSize: '0.85rem', color: '#566573', lineHeight: '1.6'}}>
                 <strong>Autores:</strong> <br/>
                 {PROJECT_INFO.authors.map((author, index) => (
---
+++ src/components/logo.jsx
@@ -0,0 +1,10 @@
+export function Logo() {
+    return (
+        <div className="logo-cont">
+            <img src="/src/images/logo.svg" className="isotype"/>
+            <p className="logo">
+                CULTIVA
+            </p>
+        </div>
+    )
+}
